dc08f36c9d0353de48c1b2c683af38461b3deb30
Vishal Mahaveer <vishalm@ti.com>
2015-07-28 14:30:49 -0500

    wlan: add wpa_supplicant_overlay.conf file
    Add wpa_supplicant_overlay.conf file needed for
    switching to Android wpa_supplicant
    
    Change-Id: I0dc63a866ea2d78248892ce53fc4d54e05103e83
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>
    Signed-off-by: D. Andrei Măceș <Andrei.Maces@alumni.nd.edu>

--- mac80211/config/Android.mk
+++ mac80211/config/Android.mk
@@ -18,6 +18,16 @@ LOCAL_PATH := $(call my-dir)
 ########################
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := wpa_supplicant_overlay.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+########################
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := hostapd.conf
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
