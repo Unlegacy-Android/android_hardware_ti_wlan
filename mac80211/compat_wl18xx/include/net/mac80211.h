abce1d48e68a4a0672dd720214325a0cbfdd35cb
Soren Steen Christensen <soren@ssc-solutions.dk>
2016-05-02 17:36:00 +0200

    wl18xx: mac80211: Allow force queuing ieee80211 delayed work
    In later kernels, adding work to the ieee80211 workqueue while
    suspending resulted in work not getting added => Wilink7/8 chips
    don't properly enter ELP sleep (see wl1271_ps_elp_sleep/elp_work).
    
    Add API for bypassing ieee80211_can_queue_work() where appropriate.
    
    Change-Id: I6433fa8b02cd6d72020f446d214e44bf98336267
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- mac80211/compat_wl18xx/include/net/mac80211.h
+++ mac80211/compat_wl18xx/include/net/mac80211.h
@@ -4642,6 +4642,20 @@ void ieee80211_queue_delayed_work(struct ieee80211_hw *hw,
 				  unsigned long delay);
 
 /**
+ * ieee80211_force_queue_delayed_work - add work onto the mac80211 workqueue
+ *
+ * Drivers and mac80211 use this to queue delayed work onto the mac80211
+ * workqueue despite system going into suspend.
+ *
+ * @hw: the hardware struct for the interface we are adding work for
+ * @dwork: delayable work to queue onto the mac80211 workqueue
+ * @delay: number of jiffies to wait before queueing
+ */
+void ieee80211_force_queue_delayed_work(struct ieee80211_hw *hw,
+				  struct delayed_work *dwork,
+				  unsigned long delay);
+
+/**
  * ieee80211_start_tx_ba_session - Start a tx Block Ack session.
  * @sta: the station for which to start a BA session
  * @tid: the TID to BA on.
